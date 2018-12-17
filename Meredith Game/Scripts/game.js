f40808d6cf35b17618efce89ed2183dded5fb0bc
Edward Pingel <ed.pingel15@gmail.com>
2018-12-17 18:11:34 +0700

    Add project files.

---
+++ Meredith Game/Scripts/game.js
@@ -0,0 +1,7 @@
+﻿var animation = bodymovin.loadAnimation({
+    container: document.getElementById('meredith'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'Assets/data.json'
+});
